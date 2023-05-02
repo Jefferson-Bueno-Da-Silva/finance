46d48e1c9f62182f3ce4491c1c2712728b870124
Jefferson Bueno Da Silva <jefferson.bueno@pixter.com.br>
2023-05-02 17:51:43 -0300

    feat: add colors

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,27 +1,37 @@
 import styled from "styled-components/native";
 import { Text } from "react-native";
 
-export const Header1 = styled(Text)`
+interface TextProps {
+  color?: string;
+}
+
+export const Header1 = styled(Text)<TextProps>`
   font-family: Inter-SemiBold;
   font-size: 24px;
+  color: ${(props) => props.color || props.theme.primary.black};
 `;
-export const Header2 = styled(Text)`
+export const Header2 = styled(Text)<TextProps>`
   font-family: Inter-Bold;
   font-size: 24px;
+  color: ${(props) => props.color || props.theme.primary.black};
 `;
-export const Header3 = styled(Text)`
+export const Header3 = styled(Text)<TextProps>`
   font-family: Inter-ExtraBold;
   font-size: 18px;
+  color: ${(props) => props.color || props.theme.primary.black};
 `;
-export const Header4 = styled(Text)`
+export const Header4 = styled(Text)<TextProps>`
   font-family: Inter-Bold;
   font-size: 16px;
+  color: ${(props) => props.color || props.theme.primary.black};
 `;
-export const Body1 = styled(Text)`
+export const Body1 = styled(Text)<TextProps>`
   font-family: Inter-Medium;
   font-size: 12px;
+  color: ${(props) => props.color || props.theme.primary.black};
 `;
-export const Label1 = styled(Text)`
+export const Label1 = styled(Text)<TextProps>`
   font-family: Inter-Regular;
   font-size: 10px;
+  color: ${(props) => props.color || props.theme.primary.black};
 `;

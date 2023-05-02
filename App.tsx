d33d635975e310cc9f7455c5a6048be7237ba0ce
Jefferson Bueno Da Silva <jefferson.bueno@pixter.com.br>
2023-05-01 21:05:34 -0300

    feat: add fonts to project

--- App.tsx
+++ App.tsx
@@ -1,14 +1,41 @@
+import React, { useCallback } from "react";
+import { View } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { ThemeProvider } from "styled-components";
+
+import Routes from "./src/routes";
+import { MainContainer } from "./src/styles/Container";
+import { defaultTheme } from "./src/styles/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Inter-ExtraBold": require("./assets/fonts/Inter-ExtraBold.ttf"),
+    "Inter-Bold": require("./assets/fonts/Inter-Bold.ttf"),
+    "Inter-SemiBold": require("./assets/fonts/Inter-SemiBold.ttf"),
+    "Inter-Medium": require("./assets/fonts/Inter-Medium.ttf"),
+    "Inter-Regular": require("./assets/fonts/Inter-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+    <View onLayout={onLayoutRootView}>
+      <ThemeProvider theme={defaultTheme}>
+        <MainContainer>
+          <StatusBar style="dark" />
+          <Routes />
+        </MainContainer>
+      </ThemeProvider>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {},
-});

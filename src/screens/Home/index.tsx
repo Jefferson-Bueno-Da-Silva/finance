b2ff4f3215ef1384126f296e2379e4551d92aff2
Jefferson Bueno Da Silva <jefferson.bueno@pixter.com.br>
2023-05-01 21:06:50 -0300

    feat: add first screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Container } from "./styles";
+import { ListItens } from "../../components";
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <ListItens />
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+import { View } from "react-native";
+
+export const Container = styled(View)`
+  gap: 24px;
+  padding: 16px;
+`;

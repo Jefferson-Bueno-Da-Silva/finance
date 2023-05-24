830c5d2df932a3a62e2a06abddd6ecb11786d48e
Jefferson Bueno Da Silva <jefferson.bueno@pixter.com.br>
2023-05-24 09:31:16 -0300

    feat: add modal and actions

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { NativeSyntheticEvent, NativeScrollEvent } from "react-native";
 
-import { FloatButton, GraphicPie, ListItens } from "../../components";
+import { FloatButton, GraphicPie, ListItens, BigModal } from "../../components";
 import { Container } from "./styles";
+import { BigModalRefs } from "../../components/Modals/BigModal";
 
 const Home: React.FC = () => {
+  const bigModalRef = useRef<BigModalRefs>(null);
   const [showButton, setShowButton] = useState<boolean>(true);
 
   const onFloatButtonVisible = (
@@ -14,10 +16,22 @@ const Home: React.FC = () => {
     setShowButton(event.nativeEvent.contentOffset.y < 10 ? true : false);
   };
 
+  const handleOpenModal = useCallback(
+    (text: string) => {
+      bigModalRef.current?.open(text);
+    },
+    [bigModalRef]
+  );
+
   return (
     <Container>
       <ListItens onScroll={onFloatButtonVisible} header={<GraphicPie />} />
-      <FloatButton showButton={showButton} />
+      <FloatButton
+        showButton={showButton}
+        onPressGreen={() => handleOpenModal("Renda")}
+        onPressRed={() => handleOpenModal("Débito")}
+      />
+      <BigModal ref={bigModalRef} />
     </Container>
   );
 };

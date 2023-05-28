f85f8a442c3769785edd28c5c87569a1dd06f753
Jefferson Bueno Da Silva <jefferson.bueno@pixter.com.br>
2023-05-28 14:53:36 -0300

    feat: add validation

--- src/components/Forms/FormCreate/validation.ts
+++ src/components/Forms/FormCreate/validation.ts
@@ -2,6 +2,14 @@ import * as yup from "yup";
 
 export const validation = yup.object({
   name: yup.string().required("Informe um apelido para a sua renda ou débito"),
-  value: yup.string().required("Informe o valor da sua conta ou renda"),
+  value: yup
+    .string()
+    .test("value", "O valor é obrigatório", (value) => {
+      return !!value?.trim().replace(/R\$/gi, "").replace(/R/gi, "");
+    })
+    .required("Informe o valor da sua conta ou renda"),
   date: yup.string().required("informe o dia do registro"),
+  monthlyRepeat: yup
+    .boolean()
+    .required("Informe se deseja ser lembrado na data escolhida"),
 });
--- src/components/Inputs/MultipleChoiceInput/index.tsx
+++ src/components/Inputs/MultipleChoiceInput/index.tsx
@@ -1,11 +1,13 @@
 import React, { useCallback, useState } from "react";
 import { FlatList } from "react-native";
 import { useTheme } from "styled-components/native";
+import { Control, Controller } from "react-hook-form";
 
 import { CheckIcon } from "../../../assets";
 import { Body1, Label1 } from "../../../styles/fonts";
 import { Checkbox, CheckboxContainer, Container } from "./styles";
 import RenderValue from "./components";
+import { MessageError } from "../../Alerts";
 
 export type option = {
   label: string;
@@ -14,35 +16,44 @@ export type option = {
 
 interface MultipleChoiceInputProps {
   options: option[];
-  onPress: (value: option) => void;
+  errorMessage?: string;
+  control: Control<any>;
+  name: string;
 }
 
 const MultipleChoiceInput: React.FC<MultipleChoiceInputProps> = ({
   options,
-  onPress,
+  errorMessage,
+  control,
+  name,
 }) => {
   const theme = useTheme();
   const [selected, setSelected] = useState<option>();
 
-  const toggleSelectedValue = useCallback((option: option) => {
-    onPress(option);
-    setSelected(option);
-  }, []);
-
   return (
-    <Container>
-      <Body1>Repetir mensalmente ?</Body1>
-      {options.map(({ value, label }, index) => (
-        <RenderValue
-          key={label}
-          onPress={toggleSelectedValue}
-          active={value === selected?.value}
-          value={value}
-          label={label}
-          theme={theme}
-        />
-      ))}
-    </Container>
+    <Controller
+      name={name}
+      control={control}
+      render={({ field: { onChange, value } }) => (
+        <Container>
+          <Body1>Repetir mensalmente ?</Body1>
+          {options.map(({ value, label }, index) => (
+            <RenderValue
+              key={label}
+              onPress={(op) => {
+                setSelected(op);
+                onChange(op.value);
+              }}
+              active={value === selected?.value}
+              value={value}
+              label={label}
+              theme={theme}
+            />
+          ))}
+          {!!errorMessage && <MessageError errorMessage={errorMessage} />}
+        </Container>
+      )}
+    />
   );
 };
 
